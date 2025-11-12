50a5955d60a0f773cecd5c078900ea80edb9858b
JIMXPS13\jimwo <jimwooley@hotmail.com>
2025-11-11 22:28:39 -0500

    Bump to .Net 10

--- ConsoleApp1/Person.cs
+++ ConsoleApp1/Person.cs
@@ -1,16 +1,15 @@
 ﻿using CsvIncrementalSerializer;
 
-namespace ConsoleApp1
-{
-    [CsvIncrementalSerializable]
-    public partial class Person
-    {
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-    }
+namespace ConsoleApp1;
 
-    [CsvIncrementalSerializable]
-    public partial record TestRecord(string FirstName, string LastName)
-    {
-    }
+[CsvIncrementalSerializable]
+public partial class Person
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+}
+
+[CsvIncrementalSerializable]
+public partial record TestRecord(string FirstName, string LastName)
+{
 }
